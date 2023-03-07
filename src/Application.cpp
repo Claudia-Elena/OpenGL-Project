90a75e6ff6e38f9b4e00d01269266b52cea12650
AndreeaDraghici <draghici108@gmail.com>
2023-03-07 22:14:52 +0200

    prepare the environment setup

--- src/Application.cpp
+++ src/Application.cpp
@@ -9,7 +9,7 @@ int main(void)
         return -1;
 
     /* Create a windowed mode window and its OpenGL context */
-    window = glfwCreateWindow(640, 480, "Triangle Pop-up by AndDrg", NULL, NULL);
+    window = glfwCreateWindow(640, 480, "Triangle Dialog by AndDrg", NULL, NULL);
     if (!window)
     {
         glfwTerminate();
